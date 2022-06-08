5c9430aa7b827c6ed1e2d3e8344d03fd73a4c495
Julian <Julian@DESKTOP-8LGHV5U>
2022-06-08 16:21:18 -0500

    Carga inicial

--- src/main/java/com/mycompany/tuprofemaven/TuProfeMaven.java
+++ src/main/java/com/mycompany/tuprofemaven/TuProfeMaven.java
@@ -3,10 +3,13 @@ package com.mycompany.tuprofemaven;
 public class TuProfeMaven {
 
     public static void main(String[] args) {
+        System.out.print("Lo estamos haciendo perro");
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Login().setVisible(true);
             }
+            
         });
+        
     }
 }
